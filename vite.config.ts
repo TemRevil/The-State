8b04024ed1e6b6caa6485f8a1b6f727362d56f57
Tem Revil <146881507+TemRevil@users.noreply.github.com>
2025-11-24 20:26:12 +0200

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/The-State/', // THIS MUST MATCH YOUR GITHUB REPO NAME
+  base: '/The-State/',
   build: {
     outDir: 'dist',
   }
